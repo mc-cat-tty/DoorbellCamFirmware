6d4151e2fab0ce17103b5d6feac89c52f169b6a1
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-07-26 11:15:20 +0200

    add methods to run and stop blink task

--- lib/hal/led/led.cpp
+++ lib/hal/led/led.cpp
@@ -1,11 +1,16 @@
 #include "led.hpp"
-#include <stdbool.h>
+
+#define EVER ;;
 
 using namespace hal::led;
 
-[[noreturn]] void Led::blinkTask(const void *params) {
-  while (true) {
+[[noreturn]] void Led::__blinkTask() {
+  for (EVER) {
     setState(hal::pin::State::In::TOGGLE);
     vTaskDelay(pdMS_TO_TICKS(blink_delay));
   }
+}
+
+[[noreturn]] void Led::blinkTask(void *params) {
+  static_cast<hal::led::Led *>(params)->__blinkTask();
 }
--- lib/hal/led/led.hpp
+++ lib/hal/led/led.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #include <time/udl.hpp>
 #include <pin/pin.hpp>
 #include <freertos/FreeRTOS.h>
@@ -8,13 +9,21 @@ namespace hal::led {
   class Led : hal::pin::Pin {
     private:
     int blink_delay = 0_s;
+    TaskHandle_t blink_handle = NULL;
+    std::string object_name = std::string("pin") + std::to_string(num);
+
+    [[noreturn]] void __blinkTask();
+    [[noreturn]] static void blinkTask(void *params);
 
     public:
     using hal::pin::Pin::Pin;
 
     [[nodiscard]] constexpr int getBlinkDelay() const { return blink_delay; }
     void setBlinkDelay(int delay) { blink_delay = delay; }
-  
-    [[noreturn]] void blinkTask(const void *params);
+
+    void startBlink(unsigned priority)
+      { if (!isBlinking()) xTaskCreate(blinkTask, object_name.c_str(), 512, (void*)this, priority, &blink_handle); }
+    [[nodiscard]] constexpr bool isBlinking() const { return blink_handle != NULL; };
+    void stopBlink() const { if (isBlinking()) vTaskDelete(blink_handle); }
   };
 }
