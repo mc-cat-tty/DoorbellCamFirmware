4eac535e7a29709d1671fc0b7110149c941c4e1c
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-11-29 13:51:56 +0100

    add mock spinner

---
+++ lib/app/animation/spinner.cpp
@@ -0,0 +1,23 @@
+#include "spinner.hpp"
+#include <common.hpp>
+#include <log/log.hpp>
+
+using namespace app::animation;
+
+constexpr static const auto mod = wrapper::log::Module::ANIMATION;
+
+void SpinnerLogMock::animate() {
+  static const auto logger = wrapper::log::Logger::getInstance();
+
+  const size_t selectionCount = pow(2ULL, demux.getSelCount());
+  logger.log(mod, ESP_LOG_DEBUG, "selCount: %d", selectionCount);
+  
+  if (currentSelection < selectionCount) {
+    demux.select(currentSelection++);
+    logger.log(mod, ESP_LOG_DEBUG, "Selecting bit number: %d", currentSelection);
+    return;
+  }
+
+  running = false;
+  logger.log(mod, ESP_LOG_DEBUG, "Stop running");
+}
